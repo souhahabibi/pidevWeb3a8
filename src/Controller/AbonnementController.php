ee9d6c326a05f475354785cd72f1b3fb968ffe0c
cyrine67 <cyrine.chalghoumi@esprit.tn>
2024-03-28 17:02:06 +0100

    push2 abonnement

--- src/Controller/AbonnementController.php
+++ src/Controller/AbonnementController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Abonnement;
+use App\Entity\Salle;
 use App\Form\AbonnementType;
 use App\Repository\SalleRepository;
+use App\Repository\AbonnementRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,18 +16,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AbonnementController extends AbstractController
 {
     #[Route('/abonnement', name: 'app_abonnement')]
-    public function index(): Response
+    public function index(AbonnementRepository $repo): Response
     {
+        $list = $repo->findAll(); 
         return $this->render('abonnement/index.html.twig', [
-            'controller_name' => 'AbonnementController',
+            'list' => $list
         ]);
     }
 
     #[Route('/Abonnement/add{id}', name: 'app_abonnement_add')]
     public function add(Request $req,ManagerRegistry $manager,$id,SalleRepository $repos): Response
     {
-       $abonnement = new Abonnement();
-       $salle = $repos->find($id);
+       $abonnement = new Abonnement(); 
+       $salle = $repos->find($id); 
        $form = $this->createForm(AbonnementType::class,$abonnement);
 
        $em = $manager->getManager();
@@ -35,13 +38,46 @@ class AbonnementController extends AbstractController
        if($form->isSubmitted() && $form->isValid())
        {
         $abonnement->setFkIdsalle($salle);
-       $em->persist($abonnement);
-       $em->flush();
+        $em->persist($abonnement);
+        $em->flush();
        return $this->redirectToRoute('app_salle_abonnement', ['id' => $id]);
        }
 
         return $this->renderForm('abonnement/abonnementForm.html.twig',
          ['f' => $form]     
         );
+    }
+    #[Route('/Abonnement/modify{id}', name: 'app_abonnement_modify')]
+    public function modify(Request $req,ManagerRegistry $manager,AbonnementRepository $repo,$id): Response
+    {
+        $abonnement = new Abonnement();
+        $abonnement = $repo->find($id);
+        $form = $this->createForm(AbonnementType::class,$abonnement);
+ 
+        $em = $manager->getManager();
+ 
+        $form->handleRequest($req); 
+ 
+        if($form->isSubmitted() && $form->isValid())
+        { 
+        $em->persist($abonnement);
+        $em->flush();
+        return $this->redirectToRoute('app_salle_abonnement', ['id' => $abonnement->getFkIdsalle()->getId()]);
+        }
+ 
+        return $this->renderForm('abonnement/abonnementForm.html.twig',
+        ['f' => $form]     
+      );
+    }
+
+    #[Route('/Abonnement/delete{id}', name: 'app_abonnement_delete')]
+    public function delete(ManagerRegistry $manager,AbonnementRepository $repo,$id): Response
+    {
+        $abonnement = $repo->find($id);
+        $em = $manager->getManager();
+ 
+        $em->remove($abonnement);
+        $em->flush();
+        return $this->redirectToRoute('app_abonnement');
     }
 }
